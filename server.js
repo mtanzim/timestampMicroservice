87ac621531381d180d6dc8963cd4f57327bb3efc
Glitch (fcctimestampmicroservice) <none>
2017-12-13 08:27:09 +0000

    🚁🖼 Checkpoint server.js:590980 test.js:590980

--- server.js
+++ server.js
@@ -38,6 +38,11 @@ app.route('/')
 		  res.sendFile(process.cwd() + '/views/index.html');
     })
 
+app.route('/test')
+    .get(function(req, res) {
+		  res.end('Hello World');
+    })
+
 // Respond not found to all the wrong routes
 app.use(function(req, res, next){
   res.status(404);
