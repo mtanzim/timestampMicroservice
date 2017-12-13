65a6c2fa3cae7dd5d6e9e08942dca4fda27dba7e
Glitch (fcctimestampmicroservice) <none>
2017-12-13 08:15:54 +0000

    need to learn how to connect scripts for url parsing and opeartions

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
-console.log('Testing...');
+console.log("Hello World!");
+/*
+var express = require('express')
+var app = express();
+app.get('/home', function(req, res) {
+  res.end('Hello World!')
+})
+app.listen(process.env.PORT, function () {
+  console.log('Node.js listening ...');
+});
+*/
